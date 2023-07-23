34d7468bf2b2ce33eaeb5a330f8ef95beba626e6
Jatin Singh Bhandari <102027407+dreamcatstudios@users.noreply.github.com>
2023-07-23 23:12:20 +0530

    main

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="flex flex-col mt-auto pt-10 w-full">
-      <footer>
+      <footer className="w-full">
         <div className="w-full bg-[#21BF73] p-5 text-center">
           <p className="text-white font-bold">
             Made by Jatin Bhandari 🧑‍💻 with ❤️
