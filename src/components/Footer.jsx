45e88be94d5d4c55fba994268784207509e14c4d
Jatin Singh Bhandari <102027407+dreamcatstudios@users.noreply.github.com>
2023-07-23 23:10:54 +0530

    main

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col mt-auto pt-10 ">
+    <div className="flex flex-col mt-auto pt-10 w-full">
       <footer>
         <div className="w-full bg-[#21BF73] p-5 text-center">
           <p className="text-white font-bold">
